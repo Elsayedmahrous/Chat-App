b5956605a5013759e00bf99198711ab1e02c2085
Elsayed <elsayed.r.mahrous@gmail.com>
2025-03-04 17:43:31 +0200

    add new route call auth

--- Routes/index.js
+++ Routes/index.js
@@ -1,7 +1,9 @@
 const userRoute = require('./userRoute');
+const authRoute = require('./authRoute')
 
 const mountRouters = (app) => {
     app.use('/api/v1/user', userRoute);
+    app.use('/api/v1/auth', authRoute);
 }
 
 module.exports = mountRouters;
